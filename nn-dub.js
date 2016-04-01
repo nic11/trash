a860765d760de7c57670cd3db91964eae48f60dd
nic11 <niconneo@yandex.ru>
2016-04-02 02:34:24 +0500

    Update nn-dub.js

--- nn-dub.js
+++ nn-dub.js
@@ -1,3 +1,13 @@
+// ==UserScript==
+// @name         Dubtrack Voters
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       niconneo_ @dubtrack
+// @match        https://www.dubtrack.fm/join/*
+// @grant        none
+// ==/UserScript==
+
 (function() {
     'use strict';
 
